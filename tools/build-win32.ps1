26a7533e4e2bfa8421de350dcea8f1ee67a929f7
Rob Lourens <roblourens@gmail.com>
2018-10-16 21:08:13 -0700

    Update to include pcre2

--- tools/build-win32.ps1
+++ tools/build-win32.ps1
@@ -1,6 +1,7 @@
-$env:RUSTFLAGS='-C target-feature=+crt-static -Z unstable-options'
-cargo build --release --target=i686-pc-windows-msvc
-Compress-Archive -Path .\target\i686-pc-windows-msvc\release\rg.exe -DestinationPath "ripgrep-$($args[0])-win32-ia32.zip"
+$env:RUSTFLAGS='-C target-feature=+crt-static'
+$env:PCRE2_SYS_STATIC=1
+cargo build --release --target=i686-pc-windows-msvc --features 'pcre2'
+Compress-Archive -Update -Path .\target\i686-pc-windows-msvc\release\rg.exe -DestinationPath "ripgrep-$($args[0])-win32-ia32.zip"
 
-cargo build --release --target=x86_64-pc-windows-msvc
-Compress-Archive -Path .\target\x86_64-pc-windows-msvc\release\rg.exe -DestinationPath "ripgrep-$($args[0])-win32-x64.zip"
+cargo build --release --target=x86_64-pc-windows-msvc --features 'pcre2'
+Compress-Archive -Update -Path .\target\x86_64-pc-windows-msvc\release\rg.exe -DestinationPath "ripgrep-$($args[0])-win32-x64.zip"
