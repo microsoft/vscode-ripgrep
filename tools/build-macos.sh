c21c09e8c910480de20d21db1cee779b96a5491a
Rob Lourens <roblourens@gmail.com>
2019-07-01 20:03:37 -0700

    Delete obsolete build scripts

--- tools/build-macos.sh
+++
@@ -1,4 +0,0 @@
-PCRE2_SYS_STATIC=1 cargo build --release --features 'pcre2'
-strip ./target/release/rg
-
-zip -j "ripgrep-$1-darwin-x64.zip" ./target/release/rg
--- tools/build-win32.ps1
+++
@@ -1,7 +0,0 @@
-$env:RUSTFLAGS='-C target-feature=+crt-static'
-$env:PCRE2_SYS_STATIC=1
-cargo build --release --target=i686-pc-windows-msvc --features 'pcre2'
-Compress-Archive -Update -Path .\target\i686-pc-windows-msvc\release\rg.exe -DestinationPath "ripgrep-$($args[0])-win32-ia32.zip"
-
-cargo build --release --target=x86_64-pc-windows-msvc --features 'pcre2'
-Compress-Archive -Update -Path .\target\x86_64-pc-windows-msvc\release\rg.exe -DestinationPath "ripgrep-$($args[0])-win32-x64.zip"
